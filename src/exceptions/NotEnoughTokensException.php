1a3cf7170fb39ed202a28eb929c9c253a29b94da
MB <berthetmathieu@free.fr>
2023-10-24 09:21:14 +0200

    Try push

--- src/exceptions/NotEnoughTokensException.php
+++ src/exceptions/NotEnoughTokensException.php
@@ -7,7 +7,7 @@ use Exception;
 class NotEnoughTokensException extends Exception {
     public function __construct(string $who, string $tokensName, int $needsNumber) {
         $message = "$who have not enought $tokensName tokens to make this transaction.
-        It needs $needsNumber $tokensName tokens (or more) to perform it.";
+        It needs $needsNumber $tokensName tokens (or more) to perform it. Try again";
         parent::__construct($message);
     }
 }
